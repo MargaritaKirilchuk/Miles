a6f1dc05190e300c5e4a9b96a7b4a45b726d37a6
Margarita <kirilchuk.margarita@gmail.com>
2020-03-28 15:18:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 10_000;
+        int rubles = 20;
+        int miles = price / rubles;
+        System.out.println(miles);
+    }
+}
